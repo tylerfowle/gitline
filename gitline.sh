2fd7067fbd5b963a11d462f7585f4107f4ff1f80
tylerfowle <tylerfowle@gmail.com>
2017-06-23 17:07:17 -0500

    add dynamic qauser

--- gitline.sh
+++ gitline.sh
@@ -77,7 +77,17 @@ function config() {
 
 
 
+# usage
+# getCreator
+# This call lists all the available assignees to which issues may be assigned. (repo)
+function getCreator() {
+  getCreator=$(curl --silent -u ${TOKEN}:x-oauth-basic ${DOMAIN}/repos/${OWNER}/${REPO}/issues/${issueNumber} )
+  creatorUsername=$(echo "$getCreator" | jq .user.login)
+  echo "$creatorUsername"
 
+  # set qa github username
+  echo "QAUSER=$creatorUsername" >> $CONFIG_FILE
+}
 
 
 # usage
@@ -160,6 +170,7 @@ function sendQA() {
 
   assignee_or_label=${GHUSER}
   removeAssignee
+  getCreator
   assignee_or_label=${QAUSER}
   addAssignee
 }
@@ -266,14 +277,14 @@ function userSelectFile() {
   #       "qa")
   #         selectedFile=$QA_FILE
   #         break;;
-  #       "milestones")
+#       "milestones")
   #         selectedFile=$MILESTONES_FILE
   #         break;;
-  #       *) echo invalid option;;
-  #     esac
-  #   done
-  #   echo '#################################'
-  # fi
+#       *) echo invalid option;;
+#     esac
+#   done
+#   echo '#################################'
+# fi
 }
 
 ########################################################################################################################
@@ -290,235 +301,235 @@ function helpCommand() {
   case $1 in
     ###########################################################
     addLabel )
-      echo '
-      Description:
-        -add a single label to a specific issue number
+    echo '
+    Description:
+    -add a single label to a specific issue number
 
-      Usage:
-        gitline addLabel [Issue Number] [Label to add]
-      '
+    Usage:
+    gitline addLabel [Issue Number] [Label to add]
+    '
     ;;
     ############################################################
     removeLabel )
-      echo '
-      Description:
-        -remove a single label from a specific issue number
+    echo '
+    Description:
+    -remove a single label from a specific issue number
 
-      Usage:
-        gitline removeLabel [Issue Number] [Label to remove]
-      '
+    Usage:
+    gitline removeLabel [Issue Number] [Label to remove]
+    '
     ;;
     ############################################################
     addAssignee )
-      echo '
-      Description:
-        -add an assignee to a specific issue number
+    echo '
+    Description:
+    -add an assignee to a specific issue number
 
-      Usage:
-        gitline addAssignee [Issue Number] [Assignee to add]
-      '
+    Usage:
+    gitline addAssignee [Issue Number] [Assignee to add]
+    '
     ;;
     ############################################################
     removeAssignee )
-      echo '
-      Description:
-        -remove an assignee from a specific issue number
+    echo '
+    Description:
+    -remove an assignee from a specific issue number
 
-      Usage:
-        gitline removeAssignee [Issue Number] [Assignee to remove]
-      '
+    Usage:
+    gitline removeAssignee [Issue Number] [Assignee to remove]
+    '
     ;;
     ############################################################
     listAssignees )
-      echo '
-      Description:
-        -list all assignees availble to assigne in this repo
+    echo '
+    Description:
+    -list all assignees availble to assigne in this repo
 
-      Usage:
-        gitline listAssignees
-      '
+    Usage:
+    gitline listAssignees
+    '
     ;;
     ############################################################
     listLabels )
-      echo '
-      Description:
-        -list all labels currently applied to a specific issue number
+    echo '
+    Description:
+    -list all labels currently applied to a specific issue number
 
-      Usage:
-        gitline listLabels [Issue Number]
-      '
+    Usage:
+    gitline listLabels [Issue Number]
+    '
     ;;
     ############################################################
     addComment )
-      echo '
-      Description:
-        -add a comment to a specific issue number
+    echo '
+    Description:
+    -add a comment to a specific issue number
 
-      Usage:
-        gitline addComment [Issue Number] ["Comment in quotes"]
-      '
+    Usage:
+    gitline addComment [Issue Number] ["Comment in quotes"]
+    '
     ;;
     ############################################################
     addMilestone )
-      echo '
-      Description:
-        -add a milestone to a specific issue number
+    echo '
+    Description:
+    -add a milestone to a specific issue number
 
-      Usage:
-        gitline addComment [Issue Number] [Milestone ID]
-      '
+    Usage:
+    gitline addComment [Issue Number] [Milestone ID]
+    '
     ;;
     ############################################################
     sendQA )
-      echo '
-      Description:
-        -send a specific issue number to QA. this includes:
-          removing "InProgress" label
-          adding "InReview" label
-          removing self as assignee
-          adding qa user as assignee
+    echo '
+    Description:
+    -send a specific issue number to QA. this includes:
+    removing "InProgress" label
+    adding "InReview" label
+    removing self as assignee
+    adding qa user as assignee
 
-      Usage:
-        gitline sendQA [Issue Number]
-      '
+    Usage:
+    gitline sendQA [Issue Number]
+    '
     ;;
     ############################################################
     sendProgress )
-      echo '
-      Description:
-        -add "InProgress" label to a specific issue number
+    echo '
+    Description:
+    -add "InProgress" label to a specific issue number
 
-      Usage:
-        gitline sendProgress [Issue Number]
-      '
+    Usage:
+    gitline sendProgress [Issue Number]
+    '
     ;;
     ############################################################
     arrayAdd )
-      echo '
-      Description:
-        -add issue number to array for using later in loop
+    echo '
+    Description:
+    -add issue number to array for using later in loop
 
-      Usage:
-        gitline arrayAdd [Issue Number]
-      '
+    Usage:
+    gitline arrayAdd [Issue Number]
+    '
     ;;
     ############################################################
     arrayRemove )
-      echo '
-      Description:
-        -remove issue number from array for using later in loop
+    echo '
+    Description:
+    -remove issue number from array for using later in loop
 
-      Usage:
-        gitline arrayRemove [Issue Number]
-      '
+    Usage:
+    gitline arrayRemove [Issue Number]
+    '
     ;;
     ############################################################
     status )
-      echo '
-      Description:
-        -print all issue numbers currently in array
+    echo '
+    Description:
+    -print all issue numbers currently in array
 
-      Usage:
-        gitline status
-      '
+    Usage:
+    gitline status
+    '
     ;;
     ############################################################
     arrayClear )
-      echo '
-      Description:
-        -remove all issues from array
+    echo '
+    Description:
+    -remove all issues from array
 
-      Usage:
-        gitline arrayClear
-      '
+    Usage:
+    gitline arrayClear
+    '
     ;;
     ############################################################
     loop )
-      echo '
-      Description:
-        -loop through issue number array and do either: sendQA or addMilestone
-          run "gitline help sendQA" for more information
-          run "gitline help addMilestone" for more information
+    echo '
+    Description:
+    -loop through issue number array and do either: sendQA or addMilestone
+    run "gitline help sendQA" for more information
+    run "gitline help addMilestone" for more information
 
-      Usage:
-        gitline loop
-      '
+    Usage:
+    gitline loop
+    '
     ;;
     ############################################################
     ac )
-      echo '
-      Description:
-        -add and commit a message to git appended with an issue number.
-        -add "InProgress" label to issue
-        -add issue number to array for use later in loop.
-          run "gitline help sendProgress" for more information
+    echo '
+    Description:
+    -add and commit a message to git appended with an issue number.
+    -add "InProgress" label to issue
+    -add issue number to array for use later in loop.
+    run "gitline help sendProgress" for more information
 
-      Usage:
-        gitline ac [Issue Number] ["Comment to commit on github in quotes"]
-      '
+    Usage:
+    gitline ac [Issue Number] ["Comment to commit on github in quotes"]
+    '
     ;;
     ############################################################
     help )
-      echo '
-      Description:
-        -learn more about gitline, optional: add a [specific command]
+    echo '
+    Description:
+    -learn more about gitline, optional: add a [specific command]
 
-      Usage:
-        gitline -h
-        gitline -h [specificCommand]
-        gitline help
-        gitline help [specific command]
-      '
+    Usage:
+    gitline -h
+    gitline -h [specificCommand]
+    gitline help
+    gitline help [specific command]
+    '
     ;;
     ############################################################
     config )
-      echo '
-      Description:
-        -add config settings to config file
+    echo '
+    Description:
+    -add config settings to config file
 
-      There are two methods for setting config options.
-      --------------------------------------------------------------------------------------------------
-      Basic - this requires parameters to be in the specific order. to add a new [repo] you
-      would have to also specify all previous parameters in the correct order.
-      Usage:
-        gitline config [token] [owner] [repo] [github username] [qa username]
-      --------------------------------------------------------------------------------------------------
-      Advanced - this method can take any number of parameters in an order.
-      Usage:
-        gitline --token=[token] --owner=[owner] --repo=[repo] --username=[your username] --qausername[qa username]
-      --------------------------------------------------------------------------------------------------
-      '
+    There are two methods for setting config options.
+    --------------------------------------------------------------------------------------------------
+    Basic - this requires parameters to be in the specific order. to add a new [repo] you
+    would have to also specify all previous parameters in the correct order.
+    Usage:
+    gitline config [token] [owner] [repo] [github username] [qa username]
+    --------------------------------------------------------------------------------------------------
+    Advanced - this method can take any number of parameters in an order.
+    Usage:
+    gitline --token=[token] --owner=[owner] --repo=[repo] --username=[your username] --qausername[qa username]
+    --------------------------------------------------------------------------------------------------
+    '
     ;;
     ############################################################
 
 
     ############################################################
     * )
-      echo '
-      Commands:
-        help
-        config
-        addLabel
-        removeLabel
-        addAssignee
-        removeAssignee
-        listAssignees
-        listLabels
-        addComment
-        addMilestone
-        sendQA
-        sendProgress
-        arrayAdd
-        arrayRemove
-        status
-        arrayClear
-        loop
-        ac
+    echo '
+    Commands:
+    help
+    config
+    addLabel
+    removeLabel
+    addAssignee
+    removeAssignee
+    listAssignees
+    listLabels
+    addComment
+    addMilestone
+    sendQA
+    sendProgress
+    arrayAdd
+    arrayRemove
+    status
+    arrayClear
+    loop
+    ac
 
-      Run "gitline help [command]" for specific usage
-      '
+    Run "gitline help [command]" for specific usage
+    '
     ;;
-  esac
+esac
 }
 
 ########################################################################################################################
@@ -527,6 +538,7 @@ function helpCommand() {
 
 # Determining Which Method To Call Based On Command Line Arguments
 case $whichMethod in
+  gc             ) getCreator ;;
   addLabel       ) addLabel ;;
   addAssignee    ) addAssignee ;;
   addcomment     ) addcomment ;;
@@ -555,13 +567,13 @@ case $whichMethod in
     setGH_Username=$5
     setQA_Username=$6;
     config
-  ;;
+    ;;
   * )
     echo '
     ERROR: invalid command
     run "gitline help" for a list of available commands.
     '
-   ;;
+    ;;
 esac
 
 
@@ -574,42 +586,42 @@ while getopts :-: arg; do
   case $arg in
     # c )  ARG_C=$OPTARG; echo '-c was selected' $OPTARG ;;
     - ) LONG_OPTARG="${OPTARG#*=}"
-        case $OPTARG in
-          username=?* )
-            echo 'GHUSER='${LONG_OPTARG} >> $CONFIG_FILE;
-            echo 'added github [your username] to config';
-            ;;
-          token=?* )
-            echo 'TOKEN='${LONG_OPTARG} >> $CONFIG_FILE;
-            echo 'added [token] to config';
-            ;;
-          owner=?* )
-            echo 'OWNER='${LONG_OPTARG} >> $CONFIG_FILE;
-            echo 'added github [organization or owner] to config';
-            ;;
-          repo=?* )
-            echo 'REPO='${LONG_OPTARG} >> $CONFIG_FILE;
-            echo 'added github [repo] to config';
-            ;;
-          qausername=?* )
-            echo 'QAUSER='${LONG_OPTARG} >> $CONFIG_FILE;
-            echo 'added github [qa person username] to config';
-            ;;
+      case $OPTARG in
+        username=?* )
+          echo 'GHUSER='${LONG_OPTARG} >> $CONFIG_FILE;
+          echo 'added github [your username] to config';
+          ;;
+        token=?* )
+          echo 'TOKEN='${LONG_OPTARG} >> $CONFIG_FILE;
+          echo 'added [token] to config';
+          ;;
+        owner=?* )
+          echo 'OWNER='${LONG_OPTARG} >> $CONFIG_FILE;
+          echo 'added github [organization or owner] to config';
+          ;;
+        repo=?* )
+          echo 'REPO='${LONG_OPTARG} >> $CONFIG_FILE;
+          echo 'added github [repo] to config';
+          ;;
+        qausername=?* )
+          echo 'QAUSER='${LONG_OPTARG} >> $CONFIG_FILE;
+          echo 'added github [qa person username] to config';
+          ;;
 
-          username*   ) echo "No arg allowed for --OPTARG options"; exit 2;;
-          token*      ) echo "No arg allowed for --OPTARG options"; exit 2;;
-          owner*      ) echo "No arg allowed for --OPTARG options"; exit 2;;
-          repo*       ) echo "No arg allowed for --OPTARG options"; exit 2;;
-          qausername* ) echo "No arg allowed for --OPTARG options"; exit 2;;
+        username*   ) echo "No arg allowed for --OPTARG options"; exit 2;;
+        token*      ) echo "No arg allowed for --OPTARG options"; exit 2;;
+        owner*      ) echo "No arg allowed for --OPTARG options"; exit 2;;
+        repo*       ) echo "No arg allowed for --OPTARG options"; exit 2;;
+        qausername* ) echo "No arg allowed for --OPTARG options"; exit 2;;
 
-           # bravo=?* )  ARG_B="$LONG_OPTARG"; echo '--bravo was selected' ;;
-           # bravo*   )  echo "No arg for --$OPTARG option" exit 2 ;;
-           # charlie  )  ARG_C=true; echo '--charlie was selected' ;;
-           # alpha* | charlie* )
-           #             echo "No arg allowed for --$OPTARG option"; exit 2 ;;
-           '' )        break ;; # "--" terminates argument processing
-           * )         echo "Illegal option --$OPTARG"; exit 2 ;;
-         esac ;;
+        # bravo=?* )  ARG_B="$LONG_OPTARG"; echo '--bravo was selected' ;;
+        # bravo*   )  echo "No arg for --$OPTARG option" exit 2 ;;
+        # charlie  )  ARG_C=true; echo '--charlie was selected' ;;
+        # alpha* | charlie* )
+          #             echo "No arg allowed for --$OPTARG option"; exit 2 ;;
+        '' )        break ;; # "--" terminates argument processing
+        * )         echo "Illegal option --$OPTARG"; exit 2 ;;
+      esac ;;
     \? ) exit 2 ;;  # getopts already reported the illegal option
   esac
 done
